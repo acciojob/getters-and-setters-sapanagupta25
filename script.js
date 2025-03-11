3f41e10a7266d41235b5c1e09130a5bedf1cd744
sapanagupta25 <146055257+sapanagupta25@users.noreply.github.com>
2025-03-11 23:12:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class Person {
+export class Person {
   private _name: string;
   private _age: number;
 
@@ -20,19 +20,19 @@ class Person {
   }
 }
 
-class Student extends Person {
+export class Student extends Person {
   study(): void {
     console.log(`${this.name} is studying`);
   }
 }
 
-class Teacher extends Person {
+export class Teacher extends Person {
   teach(): void {
     console.log(`${this.name} is teaching`);
   }
 }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+// Attach classes to window object properly
+window['Person'] = Person;
+window['Student'] = Student;
+window['Teacher'] = Teacher;
