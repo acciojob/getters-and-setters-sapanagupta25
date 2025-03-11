1b1757f5b7be83d378a590d5465f985d70696907
sapanagupta25 <146055257+sapanagupta25@users.noreply.github.com>
2025-03-11 23:20:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,32 +7,37 @@ class Person {
     this._age = age;
   }
 
+  // Getter for name
   get name(): string {
     return this._name;
   }
 
+  // Getter for age
   get age(): number {
     return this._age;
   }
 
+  // Setter for age
   set age(age: number) {
     this._age = age;
   }
 }
 
 class Student extends Person {
+  // Method to log that the student is studying
   study(): void {
     console.log(`${this.name} is studying`);
   }
 }
 
 class Teacher extends Person {
+  // Method to log that the teacher is teaching
   teach(): void {
     console.log(`${this.name} is teaching`);
   }
 }
 
-// Attach classes to the window object
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+// Attach classes to the window object (required for Cypress tests)
+(window as any).Person = Person;
+(window as any).Student = Student;
+(window as any).Teacher = Teacher;
